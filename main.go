2243859ca584c7eff1c80b48cf844cba55e53d6d
Adam <sector-f@users.noreply.github.com>
2022-06-23 04:24:04 -0400

    Implement most of code

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"hash/fnv"
 	"io"
 	"os"
+	"strconv"
 )
 
 // TODO: maybe pick a better name for this type
@@ -19,6 +20,14 @@ const (
 
 	nvcMagicBytes = "nvc1d\x00\x00\x00"
 )
+
+type entry struct {
+	Hash        uint64
+	Offset      uint32
+	_           uint32 // Uncompressed length
+	Length      uint32 // Actual length in file
+	Compression compressionType
+}
 
 func extractNVC(arcPath string, pathlist []string, outputDirectory string, extractUnknown bool) error {
 	arcFile, err := os.Open(arcPath)
@@ -49,7 +58,88 @@ func extractNVC(arcPath string, pathlist []string, outputDirectory string, extra
 		return nil
 	}
 
-	return errors.New("unimplemented")
+	entries := map[uint64]entry{}
+
+	for i := 0; i < int(numEntries); i++ {
+		var e entry
+		binary.Read(arcFile, binary.LittleEndian, &e)
+		entries[e.Hash] = e
+	}
+
+	extractedCount := 0
+
+	for hash, entry := range entries {
+		switch entry.Compression {
+		case uncompressed:
+			// Do nothing
+		case compressed:
+			// TODO: Perform zlib decompression
+		default:
+			continue
+		}
+
+		_, exists := hashToPath[hash]
+		if extractUnknown || exists {
+			_, err = arcFile.Seek(int64(entry.Offset), 0) // "0 means relative to the origin of the file"
+			if err != nil {
+				continue
+			}
+
+			data := make([]byte, entry.Length)
+			_, err = io.ReadFull(arcFile, data)
+			if err != nil {
+				continue
+			}
+
+			var path string
+
+			if exists {
+				path = hashToPath[hash]
+			} else {
+				var (
+					dirName string
+					ext     string
+				)
+
+				switch string(data[:4]) {
+				case "\x89PNG":
+					dirName = "data/unknown_png/"
+					ext = ".png"
+				case "nmf1":
+					dirName = "data/unknown_nmd/"
+					ext = ".nmd"
+				case "OggS":
+					dirName = "data/unknown_ogg/"
+					ext = ".ogg"
+				case "RIFF":
+					dirName = "data/unknown_wav/"
+					ext = ".wav"
+				default:
+					dirName = "data/unknown/"
+					ext = ".unknown"
+				}
+
+				path = dirName + strconv.FormatUint(hash, 10) + ext
+			}
+
+			outputPath := outputDirectory + path
+
+			outFile, err := os.Create(outputPath)
+			if err != nil {
+				continue
+			}
+
+			_, err = outFile.Write(data)
+			if err != nil {
+				continue
+			}
+
+			extractedCount++
+		}
+	}
+
+	fmt.Printf("Extracted %d files\n", extractedCount)
+	return nil
 }
 
 func main() {

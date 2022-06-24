3e1b6636c90985d1b9c74f74a6a426b892c5bf67
Winston Weinert <git@winny.tech>
2022-06-24 16:23:17 -0500

    pr: review

--- cmd/pathlist.go
+++ cmd/pathlist.go
@@ -11,15 +11,8 @@ import 	(
 	"github.com/spf13/cobra"
 )
 
-const (
-	regex = "data/[A-Za-z0-9_./]+\\.[a-zA-Z0-9]+"
-)
-
-var pattern regexp.Regexp
-
 func init() {
 	pathlistCmd.AddCommand(pathlistScanCmd())
-	pattern = *regexp.MustCompile(regex)
 }
 
 var pathlistCmd = &cobra.Command{
@@ -29,22 +22,26 @@ var pathlistCmd = &cobra.Command{
 
 func pathlistScanCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use: "scan",
-		Short: "Scan a core file for interesting paths",
+		Use: "scan FILE",
+		Short: "Scan a core dump for interesting paths",
 		Args: cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			verbose, _ := cmd.PersistentFlags().GetBool("verbose")
+			regex := *regexp.MustCompile("data/[A-Za-z0-9_./]+\\.[a-zA-Z0-9]+")
+
 			reader, openErr := os.Open(args[0])
-			r := bufio.NewReader(reader)
 			if openErr != nil {
 				fmt.Fprintln(os.Stderr, openErr)
 				os.Exit(1)
 			}
+			defer reader.Close()
+			r := bufio.NewReader(reader)
+
 			matches := make(map[string]struct{})
 			offset := int64(0)
 			for {  // Inspired from https://go.dev/play/p/aPrAW7XGHi
 
-				m := pattern.FindReaderIndex(r)
+				m := regex.FindReaderIndex(r)
 				if m == nil {
 					break
 				}
@@ -56,6 +53,7 @@ func pathlistScanCmd() *cobra.Command {
 				b := make([]byte, m[1]-m[0])
 				io.ReadFull(r, b)
 				s := string(b[:])
+
 				if verbose {
 					fmt.Fprintf(os.Stderr, "offset=%v s=%v\n", offset, s)
 				}
@@ -63,15 +61,17 @@ func pathlistScanCmd() *cobra.Command {
 				matches[s] = struct{}{}  // Set membership
 				offset += int64(m[1] - m[0])  // Keep record of where we think the reader is looking next.
 			}
+
 			if verbose {
 				fmt.Fprintf(os.Stderr, "\nFound %v matches.\n", len(matches))
 			}
+
 			ary := []string{}
 			for path, _ := range matches {
 				ary = append(ary, path)
 			}
-
 			sort.Strings(ary)
+
 			for _, path := range ary {
 				fmt.Println(path)
 			}

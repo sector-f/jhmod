4429c75eb8fb076f599d5047741151630d9ed003
Adam <sector-f@users.noreply.github.com>
2022-06-25 14:46:38 -0400

    Consolidate nvc package into one file

--- nvc/nvc.go
+++ nvc/nvc.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"hash/fnv"
 	"io"
 )
 
@@ -131,4 +132,17 @@ func (e TocEntry) String() string {
 		e.RawLength,
 		e.Length,
 		e.Flags)
+}
+
+// Hash is a 64-bit FNV-1a hash
+type Hash uint64
+
+func String2Hash(s string) Hash {
+	hash := fnv.New64a()
+	hash.Write([]byte(s))
+	return Hash(hash.Sum64())
+}
+
+func (h Hash) String() string {
+	return fmt.Sprintf("%016x", uint64(h))
 }

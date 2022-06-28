6860e58b7f04b37ed7db7778e6ab4316c0a3efa5
Adam <sector-f@users.noreply.github.com>
2022-06-28 11:43:26 -0400

    Use Sizeof to determine constant

--- nvc/nvc.go
+++ nvc/nvc.go
@@ -14,6 +14,7 @@ import (
 	"fmt"
 	"hash/fnv"
 	"io"
+	"unsafe"
 )
 
 // EntryFlags describes how a file is stored on disk
@@ -27,9 +28,9 @@ const (
 	// EntryFlagEncrypted indicates that the file is encrypted
 	EntryFlagEncrypted EntryFlags = 3
 
-	magic       = "nvc1d\x00\x00\x00" // NVC file type magic bytes
-	preambleLen = len(magic) + 4      // Length of magic bytes + length of ToC entry count
-	tocEntryLen = 24                  // Length of a single ToC entry
+	magic       = "nvc1d\x00\x00\x00"       // NVC file type magic bytes
+	preambleLen = len(magic) + 4            // Length of magic bytes + length of ToC entry count
+	tocEntryLen = unsafe.Sizeof(TocEntry{}) // Length of a single ToC entry
 )
 
 var ErrNoMagicFound error = errors.New("nvc magic bytes not found")
@@ -194,7 +195,7 @@ type Writer struct {
 // Finalize should be called once all files have been written to the archive (via Create or CreateCompressed).
 func NewWriter(w io.WriteSeeker, length uint32) (Writer, error) {
 	// Start by writing 0s to w until the point at which the first file will start
-	headerLen := uint32(preambleLen) + (tocEntryLen * length)
+	headerLen := uint32(preambleLen) + (uint32(tocEntryLen) * length)
 	_, err := w.Write(make([]byte, headerLen))
 	if err != nil {
 		return Writer{}, err

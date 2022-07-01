f552cd6f2c478d0b6c537ba2142485e872da8370
Adam <sector-f@users.noreply.github.com>
2022-07-01 03:11:24 -0400

    Export savefile.SaveData type

--- savefile/savefile.go
+++ savefile/savefile.go
@@ -15,7 +15,7 @@ const (
 	magic = "\xde\xc0\xad\xde"
 )
 
-type savedata struct {
+type SaveData struct {
 	// Player name
 	PlayerName string
 	// Game mode.  This can be "jh", and various others.
@@ -77,58 +77,58 @@ func discard(r io.Reader, size int) error {
 // Attempt to read in a save file.
 //
 // If the error value is non-nil, the return value does not contain valid data.
-func Parse(r io.Reader) (savedata, error) {
+func Parse(r io.Reader) (SaveData, error) {
 	z, zErr := zlib.NewReader(r)
 	if zErr != nil {
-		return savedata{}, zErr
+		return SaveData{}, zErr
 	}
 	uncompressed, uErr := ioutil.ReadAll(z)
 	if uErr != nil {
-		return savedata{}, uErr
+		return SaveData{}, uErr
 	}
 	b := bytes.NewReader(uncompressed)
 
 	header := [len(magic)]byte{}
 	if _, err := io.ReadFull(b, header[:]); err != nil {
-		return savedata{}, err
+		return SaveData{}, err
 	}
 	if string(header[:]) != magic {
-		return savedata{}, ErrNoMagicFound
+		return SaveData{}, ErrNoMagicFound
 	}
 
 	// Not sure what these bytes are, sometimes they're:
 	//  a900 0000
 	//  a200 0000
 	if err := discard(b, 4); err != nil {
-		return savedata{}, err
+		return SaveData{}, err
 	}
 
 	gameMode, gameModeErr := readString(b, 32)
 	if gameModeErr != nil {
-		return savedata{}, gameModeErr
+		return SaveData{}, gameModeErr
 	}
 
 	name, nameErr := readString(b, 32)
 	if nameErr != nil {
-		return savedata{}, nameErr
+		return SaveData{}, nameErr
 	}
 
 	cur, curErr := readString(b, 64)
 	if curErr != nil {
-		return savedata{}, curErr
+		return SaveData{}, curErr
 	}
 
 	// Not sure what these 32 bytes are...
 	if err := discard(b, 32); err != nil {
-		return savedata{}, err
+		return SaveData{}, err
 	}
 
 	seed := uint32(0)
 	if err := binary.Read(b, binary.LittleEndian, &seed); err != nil {
-		return savedata{}, err
+		return SaveData{}, err
 	}
 
-	return savedata{
+	return SaveData{
 		GameMode:     gameMode,
 		PlayerName:   name,
 		CurrentLevel: cur,

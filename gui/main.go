b4d604059cfddbd4eb139cc8239b13460bdb5ded
Winston Weinert <git@winny.tech>
2022-12-14 19:43:19 -0600

    gui: Add restore button for last save

--- gui/main.go
+++ gui/main.go
@@ -7,9 +7,10 @@ import (
 	"io"
 	"os"
 	path "path/filepath"
-	"time"
 
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
 	"github.com/sector-f/jhmod/savefile"
 	"github.com/u-root/u-root/pkg/cp"
@@ -32,8 +33,34 @@ func sha256File(path string) ([]byte, error) {
 
 func Run() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
-	lbl := widget.NewLabel("Hello World!")
+	w := a.NewWindow("jhmod manager")
+	lbl := widget.NewLabel("Last Save Will Show here")
+	var last *StoredSaveFile = nil
+	restoreLast := widget.NewButton("Restore Last", func() {
+		if last != nil {
+			last.Restore()
+		}
+	})
+	updateLast := func(f *StoredSaveFile) {
+		last = f
+		if f == nil {
+			lbl.SetText("N/A")
+			restoreLast.SetText("Restore N/A")
+			restoreLast.Disable()
+		} else {
+			restoreLast.SetText(fmt.Sprintf("Restore %s", f.OriginalBase))
+			restoreLast.Enable()
+			lbl.SetText(fmt.Sprintf("[%s] %s",
+				f.CreatedAt.Format("2006-01-02 15:04:06"),
+				f.OriginalBase))
+		}
+	}
+	content := container.New(
+		layout.NewVBoxLayout(),
+		widget.NewLabel("jhmod manager"),
+		lbl,
+		restoreLast,
+	)
 
 	mkdirErr := os.Mkdir(getSaveScumDir(), 0750)
 	if mkdirErr != nil && !os.IsExist(mkdirErr) {
@@ -53,9 +80,10 @@ func Run() {
 		panic(fmt.Sprintf("Could not migrate db.\n"))
 	}
 
-	go watchForNewSave(func(p string) {
-		lbl.SetText(fmt.Sprintf("%s %s", time.Now().String(), p))
+	db.Last(&last)
+	updateLast(last)
 
+	go watchForNewSave(func(p string) {
 		sd, err := savefile.ParseFile(p)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Could not parse save \"%s\".  Aborting.\n", p)
@@ -69,9 +97,14 @@ func Run() {
 
 		digestHex := hex.EncodeToString(digest)
 		relPath := digestHex
+		var existing *StoredSaveFile = nil
+		if db.Where("sha256_hex = ?", digestHex).Find(&existing); existing != nil {
+			fmt.Fprintf(os.Stderr, "Saw save we already have in DB, skipping (%s).\n", digestHex)
+			return
+		}
 
 		storedSaveFile := &StoredSaveFile{
-			OriginalBase:  path.Base(p), // this don't work on the winders
+			OriginalBase:  path.Base(p),
 			StoredRelPath: relPath,
 			Sha256Hex:     digestHex,
 
@@ -89,8 +122,9 @@ func Run() {
 			fmt.Printf("Copied \"%s\" to \"%s\" n saved to db.\n", p, destAbs)
 		}
 		db.Create(storedSaveFile)
+		updateLast(storedSaveFile)
 	})
 
-	w.SetContent(lbl)
+	w.SetContent(content)
 	w.ShowAndRun()
 }
--- gui/storedsavefile.go
+++ gui/storedsavefile.go
@@ -1,6 +1,10 @@
 package gui
 
 import (
+	"os"
+	path "path/filepath"
+
+	"github.com/u-root/u-root/pkg/cp"
 	"gorm.io/gorm"
 )
 
@@ -20,4 +24,20 @@ type StoredSaveFile struct {
 	CurrentLevel string
 	// The seed used to generate the game.
 	Seed uint32
+}
+
+func (f StoredSaveFile) AbsPath() string {
+	return path.Join(getSaveScumDir(), f.StoredRelPath)
+}
+
+func (f StoredSaveFile) Delete(db *gorm.DB) {
+	os.Remove(f.AbsPath())
+	db.Delete(f)
+}
+
+func (f StoredSaveFile) Restore() error {
+	return cp.Copy(
+		f.AbsPath(),
+		path.Join(guessGameDir(), f.OriginalBase),
+	)
 }
